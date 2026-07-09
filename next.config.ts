9e5449feef398703846af12ea35a64a1cf54467a
zainudin <zainudinarab@gmail.com>
2026-07-09 20:13:07 +0700

    fix: adjust next.config.ts transpilePackages to avoid conflicts and resolve jose import error on Vercel

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['firebase-admin'],
+  transpilePackages: ['jwks-rsa', 'jose'],
 };
 
 export default nextConfig;
